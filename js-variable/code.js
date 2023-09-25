eb0e6870689c748f921ce1ab3caec2f0a05e51d6
Quoc An <dangquocanf5@gmail.com>
2023-09-25 17:59:39 +0700

    done js datatype

---
+++ js-variable/code.js
@@ -0,0 +1,22 @@
+var x = 10;
+var name = "an";
+var a = 1, b = 2;
+var c = a+b;
+
+var global_var = "AN";
+
+
+
+console.log("Value of c = ", c);
+
+function writeName(){
+    window.value = 1;
+    console.log(window.value);
+    var local_var = "hihihiiiii";
+    console.log("this is local var: ", local_var);
+    console.log("this is global var: ", global_var);
+}
+
+function getValueFromOtherFunc(){
+    console.log(window.value);
+}
