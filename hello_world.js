42140fc32489425b84078bfef4fef38c18e4363f
Quoc An <dangquocanf5@gmail.com>
2023-10-30 14:30:18 +0700

    ok fill array

--- hello_world.js
+++ hello_world.js
@@ -7,3 +7,26 @@ console.log("Hello World An");
 function msg(){ // display alert box
     alert("Hello Toi la Quoc An");
 }
+
+const list = [
+    3,
+    function (a, b) {
+      return a + b
+    }
+  ]
+  const [value, func] = list
+  func(1, 2) // 3
+  console.log(func(1,2));
+  console.log(list);
+
+  const user = {
+    name: 'Dang',
+    age: 24,
+    ability: ['coding', 'depressed']
+  }
+  // shallow copy
+  const cloneUser = { ...user }
+
+  console.log(cloneUser);
+  
+  
--- js-objects/js-array/code.js
+++ js-objects/js-array/code.js
@@ -1,10 +1,10 @@
-var numbers = [1, 2,3,4,5]; // array numbers
+var numbers = [1, 2, 3, 4, 5, 6, 7]; // array numbers
 
 for (let index = 0; index < numbers.length; index++) {
-    if(numbers[index] % 2  == 0){
-        console.log(numbers[index]);
-        document.write(numbers[index] +`<br/>` );
-    }
+  if (numbers[index] % 2 == 0) {
+    console.log(numbers[index]);
+    document.write(numbers[index] + `<br/>`);
+  }
 }
 
 //create array = object array
@@ -13,12 +13,42 @@ let myPet = new Array();
 myPet[0] = `none`;
 myPet[1] = `cat`;
 myPet[2] = `dog`;
+myPet[3] = `bird`;
+myPet[4] = `dick`;
 
 for (let index = 0; index < myPet.length; index++) {
-    const element = myPet[index];
-    document.write(element + `<br/>`);
+  const element = myPet[index];
+  document.write(element + `<br/>`);
 }
 
 console.log(myPet.concat(numbers)); // merge 2 or more arrays
 
+//let copiedArray = myPet.copyWithin(3,1,2);
+//console.log(copiedArray);
 
+let itr = myPet.entries();
+
+for (let x of itr) {
+  console.log(x);
+}
+
+function test(value) {
+  return value > 10;
+}
+
+console.log(numbers.every(test));
+
+function check(element, index, array) {
+  return index < 2;
+}
+
+console.log([1, 2, 3, 4, 5, 6].every(check));
+console.log([11, 12, 23].every(check));
+
+let aFlatMap = numbers.flatMap((numbers, index) => [numbers,myPet[index]]);
+
+console.log(aFlatMap);
+
+
+let aFill = numbers.fill(1, 3);
+console.log(aFill);
